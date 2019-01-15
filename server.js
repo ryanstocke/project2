cd582af6c0e7620669d910d5d1092aedeecd21d0
ryanstocke <ryanstocke@gmail.com>
2019-01-14 21:04:02 -0700

    testing

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var db = require("./models");
 
 // Middleware
 var app = express();
-app.use(express.urlencoded({ extended: true })); 
+app.use(express.urlencoded({ extended: false })); 
 app.use(express.json());
 app.use(express.static("public"));
 //We need to use sessions to keep track of our user's login status
